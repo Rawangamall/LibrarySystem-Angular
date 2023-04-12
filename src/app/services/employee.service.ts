71710a232e2c6d1b6d52798d5b245c4d3d92920f
Rawangamall <rawan.gamaal21@gmail.com>
2023-04-12 02:17:32 +0200

    Emp authorization

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Employee } from '../models/employee';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +11,36 @@ export class EmployeeService {
   baseURL = 'http://localhost:8080/Employees/';
   baseURL2 = 'http://localhost:8080/Employee/';
 
+
   getAllEmployees(){
-    return this.http.get<Employee[]>(this.baseURL);
+    const headers = this.authService.setAuthTokenHeader();   
+    const role = this.authService.getRole();
+    if (role =="BasicAdmin"|| role =="Admin"|| role =="Owner") {
+       return this.http.get<Employee[]>(this.baseURL , { headers });
+    }else{ throw new Error('Unauthorized access: user must be an admin');}
   }
   getOneEmployee(id:number){
-    return this.http.get<Employee>(this.baseURL2+id);
+    const headers = this.authService.setAuthTokenHeader();
+
+    return this.http.get<Employee>(this.baseURL2+id , { headers });
   }
   addEmployee(emp:Employee){
-    return this.http.post<Employee>(this.baseURL,emp);
+    const headers = this.authService.setAuthTokenHeader();
+    return this.http.post<Employee>(this.baseURL,emp , { headers });
   }
   deleteEmployeeByID(id:number){
+    const headers = this.authService.setAuthTokenHeader();
     location.reload();
-    return this.http.delete(this.baseURL2+id);
+    return this.http.delete(this.baseURL2+id , { headers });
   }
   updateEmployee(emp:Employee,id:number){
+    const headers = this.authService.setAuthTokenHeader();
     console.log(this.baseURL2+id,emp);
-    return this.http.put<Employee>(this.baseURL2+id,emp);
+    return this.http.put<Employee>(this.baseURL2+id,emp , { headers });
   }
-  constructor(public http:HttpClient) {
-    this.http.get<Employee>("http://localhost:8080/Employees").subscribe(data=>{
+  constructor(public http:HttpClient , private authService: AuthService) {
+    const headers = this.authService.setAuthTokenHeader();
+    this.http.get<Employee>("http://localhost:8080/Employees", { headers }).subscribe(data=>{
       console.log(data);
     })
    }
