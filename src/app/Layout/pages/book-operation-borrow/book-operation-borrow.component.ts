4c2bb258aabe39fe7f3038272f1903faca458814
Enas <enasgamal.kh@gmail.com>
2023-04-14 14:32:52 +0200

    new

--- src/app/Layout/pages/book-operation-borrow/book-operation-borrow.component.ts
+++ src/app/Layout/pages/book-operation-borrow/book-operation-borrow.component.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Member } from 'src/app/models/member.model';
 import { Operation } from 'src/app/models/operation';
+import { MemberService } from 'src/app/services/member.service';
 import { OperationService } from 'src/app/services/operation.service';
 
 @Component({
@@ -10,15 +13,45 @@ import { OperationService } from 'src/app/services/operation.service';
 })
 export class BookOperationBorrowComponent {
   bookOper:Operation=new Operation(0,60,1,false,"");
-  constructor (public bookOperService:OperationService, private route: ActivatedRoute, public router:Router) {
+  //members: Member[] = [];
+  constructor (public bookOperService:OperationService, private route: ActivatedRoute,
+    public router:Router, public memberService:MemberService,public http:HttpClient) {
   }
   borrow(){
-    this.route.params.subscribe((params: Params) => {
-      const bookId = params['id']
-      console.log(bookId);
-      this.bookOperService.borrowBook(bookId,this.bookOper).subscribe(data => {
-        this.router.navigateByUrl('/allbook');
-      })
-    });
+    //this.http.get('/Book/borrow/:_id').subscribe(
+     // (data) => {
+        // handle successful response
+        this.route.params.subscribe((params: Params) => {
+          const bookId = params['id']
+          console.log(bookId);
+          this.bookOperService.borrowBook(bookId,this.bookOper).subscribe(data => {
+            this.router.navigateByUrl('/allbook');
+          })
+        });
+     /* },
+      (error) => {
+        if (error.status === 500) {
+          this.router.navigate(['/error500']);
+        } else if (error.status ===404) {
+          this.router.navigate(['/error404']);
+        }
+        else if (error.status ===403) {
+          this.router.navigate(['/error403']);
+        }
+        else if (error.status ===400) {
+          this.router.navigate(['/error400']);
+        }
+        else if (error.status ===503) {
+          this.router.navigate(['/error503']);
+        }
+      }
+    );*/
+    
   }
+  /*ngOnInit(){
+    this.memberService.getAllMembers().subscribe(data=>{
+      this.members = data;
+    
+    });
+  }*/
 }
