ae3ff4ef497f7bbf2448c9aa76058659c3f9438a
Enas <enasgamal.kh@gmail.com>
2023-04-14 20:40:20 +0200

    new

--- src/app/Layout/pages/bookavilablty/bookavilablty.component.ts
+++ src/app/Layout/pages/bookavilablty/bookavilablty.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/models/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-bookavilablty',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./bookavilablty.component.css']
 })
 export class BookavilabltyComponent {
-
-}
+    books:Book[]=[];
+    searchText='';
+    searchKey = '';
+    currentPage = 1;
+    constructor(public bookService:BookService, public router:Router){
+    }
+    getPages(): number[] {
+      const pageCount = Math.ceil(this.books.length / 6);
+      if (pageCount === 0) {
+        return [];
+      }
+      const pages = [];
+      for (let i = 1; i <= pageCount; i++) {
+        pages.push(i);
+      }
+      return pages;
+    }
+    delete(id:number){
+      if(confirm('Are you sure you want to delete this book?')){
+        this.bookService.deleteBook(id).subscribe(response=>
+          console.log(response));
+      }
+    }
+    search() {
+      this.bookService.searchForBook(this.searchKey, '','','').subscribe(
+        (response) => {
+          this.books = response.data;
+        },
+        (error) => {
+          if (error.status === 500) {
+            // if the status code is 500, extract the error message from the response body
+            const errorMessage = error.error.message;
+            console.error(errorMessage);
+            // clear the members array to remove the previous search results
+            this.books = [];
+          } else {
+            console.error(error);
+          }
+        }
+      );
+    }
+  
+    ngOnInit(){
+      this.bookService.getAvailableBooks().subscribe(data=>{
+        this.books = data;
+        return data;
+      })
+    }
+  }
